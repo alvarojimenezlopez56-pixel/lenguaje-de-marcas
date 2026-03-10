1af3490e4584b6c06d0b5a96a49e7f588666b331
alvarojimenezlopez56-pixel <alvarojimenezlopez56@gmail.com>
2026-03-10 12:39:40 +0100

    Actividad Final

--- ej26-30/Ejercicio_30_Landing_Automatizada/src/js/main.js
+++ ej26-30/Ejercicio_30_Landing_Automatizada/src/js/main.js
@@ -1,1 +1,23 @@
 // Lógica del menú hamburguesa
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Alternar el menú
+        nav.classList.toggle('nav-active');
+
+        // Animación de los enlaces (opcional, entrada suave)
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Animación del botón Burger (para que se convierta en una X)
+        burger.classList.toggle('toggle');
+    });
+});
